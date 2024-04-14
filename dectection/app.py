eda215464dc592989caedfa7d03b0d2422e79bd8
DanBrown47 <danwand47@gmail.com>
2024-04-14 19:00:48 +0530

    Feat : time into db

--- dectection/app.py
+++ dectection/app.py
@@ -4,24 +4,56 @@ import pika  # RabbitMQ
 from minio import Minio
 import cv2
 import numpy as np
-from color import get_color_name
+# from color import get_color_name
+import hashlib
+import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 
-
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://username:password@postgres:5432/main"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://username:password@127.0.0.1:5432/main"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
 db = SQLAlchemy(app)
 
+class Frames(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    video_id = db.Column(db.Integer, db.ForeignKey('videos.id'), nullable=False)
+    frame_path = db.Column(db.String(1000), nullable=False) # Minio Path
+    processed = db.Column(db.Boolean, default=False)
+    text = db.Column(db.String(10000), nullable=True)
+
+    def __repr__(self):
+        return '<Video %r>' % self.id
+    
+class Dectections(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    frame_id = db.Column(db.Integer, db.ForeignKey('frames.id'), nullable=False)
+    x1 = db.Column(db.Integer, nullable=False)
+    y1 = db.Column(db.Integer, nullable=False)
+    x2 = db.Column(db.Integer, nullable=False)
+    y2 = db.Column(db.Integer, nullable=False)
+    obj = db.Column(db.String(100), nullable=False)
+    color = db.Column(db.String(100), nullable=False)
+    label = db.Column(db.String(1000), nullable=False)
+    ocr = db.Column(db.String(10000), nullable=True)
+    video_name  = db.Column(db.String(1000), nullable=False)
+    snapshot_time = db.Column(db.DateTime, nullable=False)
+
+    def __repr__(self):
+        return '<Detection %r>' % self.id
 
 # TODO: This credentials is not persistent,needs to be generated multiple times
 minio_client = Minio("127.0.0.1:9000",
                      access_key="osUFvMkVb9mkNApv",
                      secret_key="YTxEZEoh9DeCYrT70xVexJn6PZgoB5i3",
                      secure=False)
+
+# TODO : Move this to a separate file
+def Calculate_hash(file):
+    return hashlib.md5(file.read()).hexdigest()
+
 
 # TODO : Move this to a separate file
 def fetch_from_minio(frame_path):
@@ -60,10 +92,6 @@ def closest_color(rgb_tuple):
     return closest_color_name
 
 def get_avg_color(image, bounding_boxes):
-    #print(image)
-    #print(type(image))
-    #image = cv2.imread(image)
-    
     # Convert BGR to RGB
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     
@@ -94,8 +122,48 @@ def crop_image_for_color(image, bbox):
     return True
   
 
-def frames_color_to_db(yolo_resp, color):
-    dectection_entry_db = 
+def get_frame_id(frame_path):
+    with app.app_context():
+        frame_path =  frame_path.decode('utf-8')
+        query = db.session.query(Frames.id).filter(Frames.frame_path == frame_path).order_by(Frames.id.desc()).limit(1)
+        last_inserted_id = query.first()
+        
+        if last_inserted_id:
+            return last_inserted_id[0]
+        else:
+            return 0
+
+def frames_color_to_db(frame_path, yolo_resp, color):
+    with app.app_context():
+        for box in yolo_resp:
+
+            fetch_frame_id = get_frame_id(frame_path)
+            x1, y1, x2, y2 = box['x1'], box['y1'], box['x2'], box['y2']
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            video_name, time = box['id'].split('/')[1:3]
+            dectected_obj = box['class']
+            color = color
+            label = box['id']
+            timestamp_str = time.split('.')[0]
+            print(timestamp_str)
+            dt = datetime.datetime.strptime(timestamp_str, '%Y-%m-%d-%H-%M-%S')
+            time_postgres_format = dt.strftime("%Y-%m-%d %H:%M:%S")
+            ocr =""
+            fetch_frame_id = get_frame_id(frame_path)
+            dectection_entry_obj = Dectections(frame_id=fetch_frame_id,
+                                               x1=x1, 
+                                               y1=y1,
+                                               x2=x2,
+                                                y2=y2,
+                                                obj=dectected_obj,
+                                                color=color,
+                                                label=label,
+                                                snapshot_time=time_postgres_format,
+                                                video_name=video_name,
+                                                ocr=ocr )
+            db.session.add(dectection_entry_obj)
+            db.session.commit()
+            print("Dectection Entry Added   ")
     return True
 
 
@@ -106,7 +174,7 @@ def process_message(ch, method, properties, body):
     image = cv2.imdecode(np.frombuffer(image_data, np.uint8), cv2.IMREAD_COLOR)  
     yolo_response = dectect_elements(body, image)
     color = get_avg_color(image, yolo_response)
-    frames_color_to_db(yolo_resp=yolo_response, color=color)
+    frames_color_to_db(frame_path=body, yolo_resp=yolo_response, color=color)
 
 
 
